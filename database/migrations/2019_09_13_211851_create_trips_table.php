e9c8d870b5a86fd7a290d29a19f306c2915d3912
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-09-13 18:31:15 -0300

    migracion trip

---
+++ database/migrations/2019_09_13_211851_create_trips_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTripsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->date('fecha');
+            $table->dateTime('hora');
+            $table->string('ciudad');
+            $table->string('latitud');
+            $table->string('longitud');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+}
