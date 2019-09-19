727c486129e975d5b907bf5bb9708414734cbc9a
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-09-18 23:57:43 -0300

    modificacion tabla users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,15 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('legajo');
+            $table->boolean('activo');
+            $table->string('telefono');
+            $table->string('usuario');
+
             $table->rememberToken();
             $table->timestamps();
         });
