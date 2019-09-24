456a96aff9af86c074d1cb20ea183ac9b4225fe3
RArielVillalobos <ariel.villalobos96@gmail.com>
2019-09-24 11:27:22 -0300

    nuevas migraciones

---
+++ database/migrations/2019_09_24_134709_create_cars_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('trip_id');
+            $table->foreign('trip_id')->references('id')->on('trips');
+            $table->string('marca');
+            $table->string('modelo');
+            $table->boolean('verificacion_tecnica');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
